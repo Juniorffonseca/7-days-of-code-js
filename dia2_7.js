2c8b5a95be12adeac7de48a0f5e3e861da937bef
jr_ff <edinaldoffjunior@gmail.com>
2022-08-23 16:17:40 -0300

    dia2

---
+++ dia2_7.js
@@ -0,0 +1,10 @@
+/* - Qual o seu nome?
+- Quantos anos você tem?
+- Qual linguagem de programação você está estudando? */
+
+var nome = prompt("Qual o seu nome?")
+var idade = prompt("Quantos anos você tem?")
+var linguagem = prompt("Qual linguagem de programação você está estudando?")
+
+
+alert("Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem + "!")
