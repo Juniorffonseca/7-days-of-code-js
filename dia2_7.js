7e25d919b3dd02ffe59297072ebf2a16a6053fc2
jr_ff <edinaldoffjunior@gmail.com>
2022-08-30 21:16:38 -0300

    ;

--- dia2_7.js
+++ dia2_7.js
@@ -8,3 +8,13 @@ var linguagem = prompt("Qual linguagem de programação você está estudando?")
 
 
 alert("Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem + "!")
+
+var resposta = prompt("Você gosta de estudar " + linguagem + " ? Responda com o número 1 para SIM ou 2 para NÃO.")
+
+if(resposta == 1){
+    alert("Muito bom! Continue estudando e você terá muito sucesso.")
+}
+
+if(resposta == 2){
+    alert("Ahh que pena... Já tentou aprender outras linguagens?")
+}
