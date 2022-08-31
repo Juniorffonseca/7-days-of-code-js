8320304c90438ee6d0b8a8958619ba288e7dd38d
jr_ff <edinaldoffjunior@gmail.com>
2022-08-30 21:01:35 -0300

    dia5.2

---
+++ dia5_7.js
@@ -0,0 +1,31 @@
+/* Então hoje, para facilitar a sua ida ao supermercado, você deve criar um programa em Javascript que perguntará se você deseja adicionar uma comida na sua lista de compras, e você deve poder responder com sim ou não.
+
+Em seguida, ele perguntará qual comida você deseja inserir, e você digitará o nome dela, como por exemplo batata.
+
+Depois, ele deverá perguntar em qual categoria essa comida se encaixa, com algumas opções já pré-definidas, como frutas, laticínios, congelados, doces e o que mais você achar interessante. Assim, você poderá separar tudo no seu devido grupo.
+
+Por fim, caso você não queira mais adicionar nada na lista de compras e responder não na primeira pergunta, ele irá exibir uma lista com todos os itens agrupados, da seguinte forma:
+
+Caso você adicione na sua lista:
+banana, leite em pó, tomate, leite vegetal, chiclete, bala de ursinho, maçã, uva, abacate e leite de vaca
+
+O programa deverá imprimir, por exemplo:
+
+Lista de compras:
+    Frutas: banana, tomate, maçã, uva, abacate
+    Laticínios: leite vegetal, leite de vaca, leite em pó
+    Congelados:
+    Doces: chiclete e bala de ursinho */
+
+const adicionar = prompt("Você gostaria de adicionar uma comida na sua lista de compras? Responda com sim ou não")
+let listaItens = [];
+
+if(adicionar == "sim")
+{
+    const comida = prompt("Qual comida você gostaria de adicionar?")
+    listaItens.push(comida)
+}
+if(adicionar == "não")
+{
+    alert(listaItens)
+}
